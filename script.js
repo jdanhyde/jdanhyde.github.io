3d813a3237650db664fca430792a4ae4e59a7057
Jordan Hyde <jdanhyde@gmail.com>
2015-12-08 09:30:21 -0800

    niiiiiiiiiiiice

--- script.js
+++ script.js
@@ -10,6 +10,28 @@ $( document ).ready(function(){
                 }
         }, 1000);
 });
+$('#castme').click(function(){
+        launchApp();
+});
+function launchApp() {
+        console.log("Launching the Chromecast App...");
+        chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
+}
+
+function onRequestSessionSuccess(e) {
+        console.log("Successfully created session: " + e.sessionId);
+        session = e;
+}
+
+function onLaunchError() {
+        console.log("Error connecting to the Chromecast.");
+}
+
+function onRequestSessionSuccess(e) {
+        console.log("Successfully created session: " + e.sessionId);
+        session = e;
+        loadMedia();
+}
 function initializeCastApi() {
         var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
         var sessionRequest = new chrome.cast.SessionRequest(applicationID);
@@ -45,4 +67,42 @@ function onInitSuccess() {
 
 function onInitError() {
         console.log("Initialization failed");
+}
+function loadMedia() {
+        if (!session) {
+                console.log("No session.");
+                return;
+        }
+
+        var mediaInfo = new
+chrome.cast.media.MediaInfo('http://i.imgur.com/IFD14.jpg');
+        mediaInfo.contentType = 'image/jpg';
+  
+        var request = new chrome.cast.media.LoadRequest(mediaInfo);
+        request.autoplay = true;
+
+        session.loadMedia(request, onLoadSuccess, onLoadError);
+}
+
+function onLoadSuccess() {
+        console.log('Successfully loaded image.');
+}
+
+function onLoadError() {
+        console.log('Failed to load image.');
+}
+$('#stop').click(function(){
+        stopApp();
+});
+
+function stopApp() {
+        session.stop(onStopAppSuccess, onStopAppError);
+}
+
+function onStopAppSuccess() {
+        console.log('Successfully stopped app.');
+}
+
+function onStopAppError() {
+        console.log('Error stopping app.');
 }
