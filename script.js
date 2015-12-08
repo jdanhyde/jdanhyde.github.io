114bb47519acda4df94b2e6b239dd16904c66810
Jordan Hyde <jdanhyde@gmail.com>
2015-12-08 09:22:55 -0800

    guess who's back
    back again

---
+++ script.js
@@ -0,0 +1,48 @@
+var session = null;
+$( document ).ready(function(){
+        var loadCastInterval = setInterval(function(){
+                if (chrome.cast.isAvailable) {
+                        console.log('Cast has loaded.');
+                        clearInterval(loadCastInterval);
+                        initializeCastApi();
+                } else {
+                        console.log('Unavailable');
+                }
+        }, 1000);
+});
+function initializeCastApi() {
+        var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
+        var sessionRequest = new chrome.cast.SessionRequest(applicationID);
+        var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+                sessionListener,
+                receiverListener);
+        chrome.cast.initialize(apiConfig, onInitSuccess, onInitError);
+};
+
+var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+                sessionListener,
+                receiverListener);
+				
+function sessionListener(e) {
+        session = e;
+        console.log('New session');
+        if (session.media.length != 0) {
+                console.log('Found ' + session.media.length + ' sessions.');
+        }
+}
+
+function receiverListener(e) {
+        if( e === 'available' ) {
+                console.log("Chromecast was found on the network.");
+        }
+        else {
+                console.log("There are no Chromecasts available.");
+        }
+}
+function onInitSuccess() {
+        console.log("Initialization succeeded");
+}
+
+function onInitError() {
+        console.log("Initialization failed");
+}
